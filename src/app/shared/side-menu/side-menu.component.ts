b6e11b9e593a3dbb579afc6748e6b6d32ef6bee3
Fabian Portilla <fas_456@hotmail.com>
2022-09-29 15:22:38 -0500

    Create basic files

--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,16 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+interface MenuItem{
+  text: string,
+  route: string
+}
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
-  styles: [
-  ]
+  styles: [`
+    li{
+      cursor: pointer;
+    }
+  `]
 })
-export class SideMenuComponent implements OnInit {
+export class SideMenuComponent   {
 
-  constructor() { }
+  public templateMenu: MenuItem[] = [
+    {
+      text:'Basics',
+      route:'./template/basics'
+    },
+    {
+      text:'Dynamics',
+      route:'./template/dynamics'
+    },
+    {
+      text:'Switches',
+      route:'./template/switches'
+    },
+  ]
 
-  ngOnInit(): void {
-  }
+  public reactiveMenu: MenuItem[] = [
+    {
+      text:'Basics',
+      route:'./reactive/basics'
+    },
+    {
+      text:'Dynamics',
+      route:'./reactive/dynamics'
+    },
+    {
+      text:'Switches',
+      route:'./reactive/switches'
+    },
+  ]
 
 }
